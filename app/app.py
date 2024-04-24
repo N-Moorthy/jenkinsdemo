a9f8b4ad8a397c245eb9a32efe913681e7d7bdea
N Moorthy <163540678+N-Moorthy@users.noreply.github.com>
2024-04-25 01:16:20 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 def colorful_header(text):
     # ANSI escape sequences for colors
-    HEADER = '<h1 style="color: #00ff00;">'
+    HEADER = '<h1 style="color: #000000;">'
     END = '</h1>'
     return f"{HEADER}{text}{END}"
 
 @app.route('/')
 def index():
-    header = colorful_header("Welcome....!")
+    header = colorful_header("Hello All & Welcome to Basic Pipeline Demo Project....!")
     details = '<h2>Here is My Basic Details</h2><p>Name: Narasimmamoorthy</p><p>Email: bala21.10moorthy@gmail.com</p><p>Role :DevOps</p>'
     return f"{header}\n{details}"
 
